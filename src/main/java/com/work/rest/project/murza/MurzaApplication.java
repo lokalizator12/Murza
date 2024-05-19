3cc0ce1b494b3812dadf0974dfa2bdf3d7d7cda5
Anatoli Fatsik <135855@student.uni.opole.pl>
2024-05-19 19:49:25 +0200

    Initial commit

---
+++ src/main/java/com/work/rest/project/murza/MurzaApplication.java
@@ -0,0 +1,13 @@
+package com.work.rest.project.murza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MurzaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MurzaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/work/rest/project/murza/MurzaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.work.rest.project.murza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MurzaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
